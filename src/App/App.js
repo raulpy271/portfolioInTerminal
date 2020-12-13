9bea26844e431913806941a3eb014e37e603287e
Raul <raulrufino0@gmail.com>
2020-12-13 17:25:17 -0300

    importing terminal component in the app and removing unecessary css properties

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import TerminalWindow from '../Terminal/Terminal.js'
+
 
 function App() {
   return (
     <div className="App">
+      <TerminalWindow/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
