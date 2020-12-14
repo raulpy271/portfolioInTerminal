c7eb50b9c66d450c08435eda4af14aaaeccfc468
Raul <raulrufino0@gmail.com>
2020-12-14 15:27:41 -0300

    adding another botton in topbar

--- src/TopBar/TopBar.js
+++ src/TopBar/TopBar.js
@@ -2,26 +2,34 @@ import './TopBar.css'
 
 
 function Button(props) {
+    let len = 10
+    let margin = len / 2
     return (
         <div className="button"
             style={{ 
                 "backgroundColor" : props.color,
-                "width"  : String(props.len) + "px",
-                "height" : String(props.len) + "px"
+                "width"  : String(len) + "px",
+                "height" : String(len) + "px",
+                "marginLeft" : String(margin) + "px"
             }}
         >
         </div>
     )
 }
 
+
 function TopBar(props) {
+    let red    = "#f00"
+    let yellow = "#ff0"
+    let green  = "#0f0"
     return (
         <div className="top_bar" >
-            <p id="title" > {props.title} </p>
             <div className="buttons">
-                <Button color="red" len={10}/>
-                <Button color="blue" len={10}/>
+                <Button color={red}/>
+                <Button color={yellow}/>
+                <Button color={green}/>
             </div>
+            <p id="title" > {props.title} </p>
         </div>
     )
 }
