4fe9284ef48eda4f38ae817498d16c1b8647a442
Raul <raulrufino0@gmail.com>
2020-12-14 16:47:33 -0300

    adding a wallpaper behind terminal

--- src/App/App.js
+++ src/App/App.js
@@ -6,19 +6,6 @@ function App() {
   return (
     <div className="App">
       <TerminalWindow/>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
