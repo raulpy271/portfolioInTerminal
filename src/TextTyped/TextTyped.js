005a1eac92e684f7a398b78dcc8805d1af02035c
Raul <raulrufino0@gmail.com>
2020-12-18 17:37:55 -0300

    adding TextTyped component. This component receives a text and shows this text as if the text is being typed in terminal

---
+++ src/TextTyped/TextTyped.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import Cursor from '../Cursor/Cursor.js';
+import './TextTyped.css'
+
+
+class TextTyped extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      timeToWaitBetweenEachChar : props.totalTime / props.children.length,
+      textInsideTerminalText : '',
+      charsToBeShowed : props.children.split('')
+    }
+  }
+
+
+  checkIfLeftCharToBeShowed = () => {
+    return Boolean(String(this.state.charsToBeShowed) !== "")
+  }
+
+
+  showOneChar = () => {
+    let [head, ...tail] = this.state.charsToBeShowed
+    let newText = this.state.textInsideTerminalText + head
+    this.setState({
+      textInsideTerminalText : newText,
+      charsToBeShowed : tail
+    })
+  }
+
+
+  showACharIfIsMissingACharToBeShown = () => {
+    if (this.checkIfLeftCharToBeShowed()) {
+      this.showOneChar()
+    } else {
+      clearInterval(this.interval)
+    }
+  }
+
+
+  componentDidMount = () => {
+    this.interval = setInterval(
+      this.showACharIfIsMissingACharToBeShown, 
+      this.state.timeToWaitBetweenEachChar
+    )
+  }
+
+
+  render() {
+    return (
+      <div className="textTyped">
+        <p id="textTyped">
+          {this.state.textInsideTerminalText}<Cursor/>
+        </p>
+      </div>
+    )
+  }
+}
+
+
+export default TextTyped
