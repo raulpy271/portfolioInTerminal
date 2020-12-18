4bb4fb2e99e7b4feb14fef268bb6e2cafb2a1b92
Raul <raulrufino0@gmail.com>
2020-12-18 17:27:27 -0300

    adding directory with Cursor component

---
+++ src/Cursor/Cursor.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import './Cursor.css'
+
+
+class Cursor extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state  = {
+      cursorChar : '█',
+      timeToUpdate   : 1000,
+      cursorOnStatus : true,
+      opacity : 100
+    }
+  }
+
+
+  setCursorStatus = (is_on) => {
+    if (is_on) {
+      this.setState({
+        cursorOnStatus : true,
+        opacity : 100
+      })
+    } else {
+      this.setState({
+        cursorOnStatus : false,
+        opacity : 0
+      })
+    }
+  }
+
+
+  changeCursorState = () => {
+    this.setCursorStatus(!this.state.cursorOnStatus)
+  }
+
+
+  componentDidMount = () => {
+    this.interval = setInterval(() => {
+      this.changeCursorState() 
+    }, this.state.timeToUpdate)
+  }
+
+
+  componentWillUnmount = () => {
+    clearInterval(this.interval)
+  }
+
+
+  render() {
+    let opacity = String(this.state.opacity) + "%"
+    return (
+      <strong id="cursor" style={{"opacity" : opacity}}>
+        {this.state.cursorChar}
+      </strong>
+    )
+  }
+}
+
+
+export default Cursor
