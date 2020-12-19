808b60fb15f4d19d87c043718bd13e0db9a74499
Raul <raulrufino0@gmail.com>
2020-12-19 13:30:39 -0300

    adding content object, these object store all text and sections used in the terminal display

---
+++ src/content.js
@@ -0,0 +1,29 @@
+
+
+var Content = {
+  "prompt" : "[raulpy271@fedora] λ ",
+  "title"  : "Terminal",
+  "sections" : {
+    "name" : "sections",
+    "sectionContent" : `Hi 👋, I'm @raulpy271, 
+What are you looking for?
+
+- About me
+- Projects
+- Skills
+- Contact
+`
+  },
+  "aboutMe" : {
+    "name" : "aboutMe",
+    "sectionContent" : `I'm a computer science student for at least one year and a programmer self-student for at least two. I like to think about how to describe real problems using programming and how to write elegant code.
+
+Moreover, I like to study programmer languages, functional programming, clean code, and talk about Linux, open-source, and tech in general. 
+
+Thanks for reading, feels free to send me a message 🤗
+`
+  }
+}
+
+
+export default Content
