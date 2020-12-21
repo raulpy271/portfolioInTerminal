0ebd83f6d3cc5c565fb14d57def0b556f5a46c9d
Raul <raulrufino0@gmail.com>
2020-12-21 13:50:48 -0300

    adding the section skills in the content file

--- src/Content.js
+++ src/Content.js
@@ -3,39 +3,48 @@
 var Content = {
   "prompt" : "[raulpy271@fedora] λ ",
   "title"  : "Terminal",
-  "sections" : {
-    "name" : "sections",
-    "sectionContent" : 
-  <>
-    Hi 👋, I'm <a href="www.github.com/raulpy271">@raulpy271</a>,<br/>
-    What are you looking for?
-  </>
-  },
-  "aboutMe" : {
+  "aboutme" : {
     "name" : "aboutMe",
-    "sectionContent" : `I'm a computer science student for at least one year and a programmer self-student for at least two. I like to think about how to describe real problems using programming and how to write elegant code.
-
+    "sectionContent" : 
+<>
+  <p>
+Hi 👋, I'm <a rel="noreferrer" target="_blank" href="https://www.github.com/raulpy271">@raulpy271</a>,<br/>
+I'm a computer science student for at least one year and a programmer self-student for at least two. I like to think about how to describe real problems using programming and how to write elegant code.
+  </p>
+  <p>
 Moreover, I like to study programmer languages, functional programming, clean code, and talk about Linux, open-source, and tech in general. 
-
-Thanks for reading, feels free to send me a message 🤗
-`
+  </p>
+</>
+  },
+  "skills" : {
+    "name" : "skills",
+    "sectionContent" :
+<>
+  <p>
+I used python to make projects about image processing, bot, and automation.
+  </p>
+  <p>
+I have known Javascript and ReactJs, because already make projects using these tools, including this page.
+  </p>
+  <p>
+Besides, I knew and liked many concepts of functional programming, where I learned from the Haskell language and javascript.
+  </p>
+  <p>
+See my <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271">github account</a> to known more about these projects.
+  </p>
+</>
   },
   "contact" : {
     "name" : "contact",
     "sectionContent" : 
   <>
-    Feels free to send me a message, I will like to talk with you.<br/>
-    Here my contact info:<br/>
-    <ul>
-      <li>Email: 
-        <a href="mailto:raulrufino0@gmail.com">raulrufino0@gmail.com</a>
-      </li>
-      <li>My github: 
-        <a href="https://github.com/raulpy271">@raulpy271</a>
-      </li>
-    </ul>
+    <p>Feels free to send me a message, I will like to talk with you.</p>
+    <p>Here my contact info:</p>
+    <p><a rel="noreferrer" target="_blank" href="https://github.com/raulpy271">My github</a></p>
+    <p><a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/raulpy271/">My Linkedin</a></p>
+    <p><a rel="noreferrer" target="_blank" href="https://t.me/raulpy271">My Telegram</a></p>
+    <p><a href="mailto:raulrufino0@gmail.com">raulrufino0@gmail.com</a></p>
   </>
-
   }
 }
 
