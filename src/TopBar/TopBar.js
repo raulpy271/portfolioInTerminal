ab56ef360c34d919dce0dc89fb013ccb9f8fac29
Raul <raulrufino0@gmail.com>
2020-12-22 17:12:49 -0300

    importing the module buttonsFunction in TopBar component. And adding the event of maximize and centralize screen

--- src/TopBar/TopBar.js
+++ src/TopBar/TopBar.js
@@ -1,18 +1,22 @@
+import {useEffect} from 'react'
 import './TopBar.css'
+import {maximizeOrCentralizeScreen} from './buttonsFunction.js'
 
 
 function Button(props) {
   let len = 12
   let margin = len / 2
   return (
-    <div className="button"
-    style={{ 
-      "color" : props.color,
-        "backgroundColor" : props.color,
-        "width"  : String(len) + "px",
-        "height" : String(len) + "px",
-        "marginLeft" : String(margin) + "px"
-    }}
+    <div 
+      className="button"
+      onClick={props.onClick}
+      style={{ 
+        "color" : props.color,
+          "backgroundColor" : props.color,
+          "width"  : String(len) + "px",
+          "height" : String(len) + "px",
+          "marginLeft" : String(margin) + "px"
+      }}
     >
     </div>
   )
@@ -20,15 +24,31 @@ function Button(props) {
 
 
 function TopBar(props) {
+  let element
   let red    = "#f00"
   let yellow = "#ff0"
   let green  = "#0f0"
+
+
+  const assigmentElement = elementFromEffect => {
+    element = elementFromEffect
+  }
+
+
+  useEffect(() => 
+    assigmentElement(document.querySelector("div.terminalDialog"))
+  )
+
+
+  const redButtonFunction = () => maximizeOrCentralizeScreen(element)
+
+
   return (
     <div className="topBar" >
     <div className="buttons">
-    <Button color={red}/>
-    <Button color={yellow}/>
-    <Button color={green}/>
+    <Button onClick={redButtonFunction} color={red}/>
+    <Button onClick={redButtonFunction} color={yellow}/>
+    <Button onClick={redButtonFunction} color={green}/>
     </div>
     <p id="title" > {props.title} </p>
     </div>
