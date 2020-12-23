d6f54ee953ed37e4ea83248c6c3ec41e3654190b
Raul <raulrufino0@gmail.com>
2020-12-23 11:03:49 -0300

    adding functions to change color of text and background

--- src/TopBar/buttonsFunction.js
+++ src/TopBar/buttonsFunction.js
@@ -1,6 +1,43 @@
 
 
-var isCentralized = true
+var ButtonStates = {
+  "isCentralized" : true,
+  "textColorsSelected" : 0,
+  "backgroundColorsSelected" : 0,
+  "textColors" : ["#0f0", "red", "yellow"],
+  "backgroundColors" : 
+    ["rgba(0, 0, 0, 90%)", 
+     "rgba(0, 0, 0, 60%)", 
+     "rgba(255, 255, 255, 90%)"
+    ]
+}
+
+
+const getNextElement = (list, index) => {
+  let length = list.length
+  if (index + 1 === length) {
+      return [list[0], 0]
+  }
+  return [list[index + 1], index + 1]
+}
+
+
+const nextBackgroundColors = colorObj => {
+  let [nextElement, nextIndex] = getNextElement(
+    colorObj["backgroundColors"],
+    colorObj["backgroundColorsSelected"])
+  colorObj["backgroundColorsSelected"] = nextIndex
+  return nextElement
+}
+
+
+const nextTextColors = colorObj => {
+  let [nextElement, nextIndex] = getNextElement(
+    colorObj["textColors"],
+    colorObj["textColorsSelected"])
+  colorObj["textColorsSelected"] = nextIndex
+  return nextElement
+}
 
 
 const maximizeScreen = element => {
@@ -19,11 +56,19 @@ const centralizeScreen = element => {
 }
 
 
+const changeTextColor = element => 
+  element.style.color = nextTextColors(ButtonStates)
+
+
+const changeBackgroundColors = element => 
+  element.style.backgroundColor = nextBackgroundColors(ButtonStates)
+
+
 const maximizeOrCentralizeScreen = element => {
-  if (isCentralized) maximizeScreen(element)
+  if (ButtonStates["isCentralized"]) maximizeScreen(element)
   else centralizeScreen(element)
-  isCentralized = ! isCentralized
+  ButtonStates["isCentralized"] = ! ButtonStates["isCentralized"]
 }
 
 
-export {maximizeOrCentralizeScreen}
+export {maximizeOrCentralizeScreen, changeTextColor, changeBackgroundColors}
