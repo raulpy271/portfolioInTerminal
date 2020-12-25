5d9f24bdf71b15b05d01bde75f3f62657237828a
Raul <raulrufino0@gmail.com>
2020-12-25 15:35:44 -0300

    importing Terminal component in the index file instead import App component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App.js';
+import TerminalWindow from './Terminal/Terminal.js'
 
 
 ReactDOM.render(
   <React.StrictMode>
-  <App />
+    <TerminalWindow/>
   </React.StrictMode>,
   document.getElementById('root')
 );
