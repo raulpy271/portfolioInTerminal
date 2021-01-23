5be0ce335421a4bd3a6ec9c6e1df84d0f68826e3
Raul <raulrufino0@gmail.com>
2021-01-23 12:58:17 -0300

    adding section "about me" in portuguese

--- src/text/contentPT.js
+++ src/text/contentPT.js
@@ -7,6 +7,19 @@ var contentPT = {
 
   "aboutme" : (
 <>
+  <p>
+Oi 👋, Eu sou <a rel="noreferrer" target="_blank" href="https://www.github.com/raulpy271">@raulpy271</a>,<br/>
+Eu sou estudante de ciência da computação por dois anos e estudante de programação a pelo menos 3. Eu gosto de programar, de pensar como desenhar software, estruturar projetos e como escrever código elegante.
+  </p>
+  <p>
+Além disso, eu gosto de estudar linguagens de programação, programação funcional, clean code e de conversar sobre Linux, open-source e tecnologia no geral.
+  </p>
+  <p>
+Eu desenvolvi projetos pessoais onde eu trabalhei com web apps, APIs, bots, e banco de dados. Eu usei conceitos de programação funcional, programação orientada a objetos, REST API e processamento de imagens.
+  </p>
+  <p>
+Veja a seção sobre minhas skills para ver meus projetos e linguagens que eu usei, ou me contate se tiver interesse.
+  </p>
 </>
   ),
 
