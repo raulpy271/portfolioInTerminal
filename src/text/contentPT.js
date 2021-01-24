e6ff0643f504d576f52a0c7b0d91a703994a6959
Raul <raulrufino0@gmail.com>
2021-01-24 14:16:39 -0300

    Change the structure of the content files
    After this, each section have a component with the content. Now, they are objects with two properties, first the section name, and the content itself.
    
    With this change, it's easy to acess the section name, and have section names with different languages.

--- src/text/contentPT.js
+++ src/text/contentPT.js
@@ -1,57 +1,59 @@
 
 
 var contentPT = {
-  "prompt" : "[raulpy271@fedora] λ ",
-  "title"  : "Terminal",
-
-
-  "aboutme" : (
+  "aboutme" : {
+    "name" : "Sobre mim",
+    "content" :
 <>
-  <p>
+<p>
 Oi 👋, Eu sou <a rel="noreferrer" target="_blank" href="https://www.github.com/raulpy271">@raulpy271</a>,<br/>
 Eu sou estudante de ciência da computação por dois anos e estudante de programação a pelo menos 3. Eu gosto de programar, de pensar como desenhar software, estruturar projetos e como escrever código elegante.
-  </p>
-  <p>
+</p>
+<p>
 Além disso, eu gosto de estudar linguagens de programação, programação funcional, clean code e de conversar sobre Linux, open-source e tecnologia no geral.
-  </p>
-  <p>
+</p>
+<p>
 Eu desenvolvi projetos pessoais onde eu trabalhei com web apps, APIs, bots, e banco de dados. Eu usei conceitos de programação funcional, programação orientada a objetos, REST API e processamento de imagens.
-  </p>
-  <p>
+</p>
+<p>
 Veja a seção sobre minhas skills para ver meus projetos e linguagens que eu usei, ou me contate se tiver interesse.
-  </p>
+</p>
 </>
-  ),
+  },
 
 
-  "skills" : (
+  "skills" : {
+    "name" : "Habilidades",
+    "content" :
 <>
-  <p>
+<p>
 Para trabalhar com processamento de imagem, bots, e automações eu usei Python. Aqui está um <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/telegram_ecommerce">bot</a> que eu escrevi, e um <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/Fractals-JuliaSet">script</a> onde usei processamento de imagens para criar um fractal.
-  </p>
-  <p>
+</p>
+<p>
 Para desenvolver web apps e APIs eu utilizei JavaScript, ReactJs e ExpressJS. Veja uma <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/DraggableDialog">biblioteca</a> que eu desenvolvi com essa linguagem.
-  </p>
-  <p>
+</p>
+<p>
 Além disso, eu conheço vários conceitos de programação funcional, onde aprendi da linguagem Haskell e JavaScript. Veja esse <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/ValidationMethods">pacote</a> onde eu usei a beleza do sistema de tipos do Haskell.
-  </p>
-  <p>
+</p>
+<p>
 Veja minha conta no <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271">Github</a> para conhecer mais sobre esses projetos.
-  </p>
+</p>
 </>
-  ),
+  },
 
 
-  "contact" : (
-  <>
-    <p>Eu estou aberto para trabalhar em projetos privados ou open-source, ou somente conversar com você, então sinta-se livre para me enviar uma mensagem, eu gostarei de conversar.</p>
-    <p>Veja minhas informações de contato:</p>
-    <p> 💻 <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271">Meu Github</a></p>
-    <p> 👔 <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/raulpy271/">Meu Linkedin</a></p>
-    <p> 📞 <a rel="noreferrer" target="_blank" href="https://t.me/raulpy271">Meu Telegram</a></p>
-    <p> 📨 <a href="mailto:raulrufino0@gmail.com">raulrufino0@gmail.com</a></p>
-  </>
-  )
+  "contact" : {
+    "name" : "Contato",
+    "content" : 
+<>
+<p>Eu estou aberto para trabalhar em projetos privados ou open-source, ou somente conversar com você, então sinta-se livre para me enviar uma mensagem, eu gostarei de conversar.</p>
+<p>Veja minhas informações de contato:</p>
+<p> 💻 <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271">Meu Github</a></p>
+<p> 👔 <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/raulpy271/">Meu Linkedin</a></p>
+<p> 📞 <a rel="noreferrer" target="_blank" href="https://t.me/raulpy271">Meu Telegram</a></p>
+<p> 📨 <a href="mailto:raulrufino0@gmail.com">raulrufino0@gmail.com</a></p>
+</>
+  }
 }
 
 
