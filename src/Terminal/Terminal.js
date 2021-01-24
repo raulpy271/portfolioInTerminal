10b5284796550fefc9cdca5d0edb2da9588c4d34
Raul <raulrufino0@gmail.com>
2021-01-24 14:34:32 -0300

    Define the terminal title inside the `TerminalWindow`
    After this, the terminal title are stored in the content files, but this file shloud store only content of the sections. So, i removed the dependence of the content files and store the title inside the Terminal component

--- src/Terminal/Terminal.js
+++ src/Terminal/Terminal.js
@@ -1,12 +1,11 @@
 import turnDraggable from '../utils/turnDraggable.js'
 import TopBar from '../TopBar/TopBar.js'
 import TerminalOutput from '../TerminalOutput/TerminalOutput.js'
-import {getSection} from '../text/textManager.js'
 import './Terminal.css'
 
 
 const TerminalWindow = () => {
-  let title = getSection("title")
+  let title = "Terminal"
   return (
     <div id="terminalDialog">
       <div>
